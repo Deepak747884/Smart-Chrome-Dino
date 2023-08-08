782af671b74dd04b62ede9c81d85c07a88453bbd
Deepak Dhingra <deepakdhingra1999@gmail.com>
2023-08-08 01:29:55 -0400

    final

---
+++ chrome_dino/server.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, make_response
+from run import run_dino
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+  return render_template('start.html')
+ 
+@app.route('/index.html')
+def other_page():
+    # Add code to handle "index.html" here if needed
+    return render_template('index.html')
+
+@app.route('/my-link/')
+def my_link():
+  run_dino()
+  response = make_response("<script>window.close();</script>")
+  return response
+
+if __name__ == '__main__':
+  app.run(debug=True)
